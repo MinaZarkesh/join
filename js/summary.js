20a05a515d8a5198223e99a78a3a5f4ecf9b6ade
Mina <mina_zarkesh@gmx.de>
2023-12-07 16:13:03 +0100

    update dynamic amount of item in Summary

--- js/summary.js
+++ js/summary.js
@@ -1,4 +1,13 @@
 function initBoard() {
   init();
-  new SummaryBox("summaryBox");
+  createSummaryBoxes();
+}
+let description= "Hier könnte dein Name stehen";
+function createSummaryBoxes(){
+    new SummaryBox("summaryBox", 0);
+    new SummaryBox("summaryBox", 1);
+    new SummaryBox("summaryBox", 2);
+    new SummaryBox("summaryBox", 3);
+    new SummaryBox("summaryBox", 4);
+    new SummaryBox("summaryBox", 5);
 }
--- models/summaryBoxes.class.js
+++ models/summaryBoxes.class.js
@@ -1,18 +1,66 @@
 class SummaryBox {
 
-    taskCounter;
-    img;
-    description;
-    item
-    constructor(id){
+    // for calcPosition
+  containerWidth = 798;
+  containerHeight = 620;
+  spanheight = 36;
+  gap = 16;
+  itemAmountPerRow = 4;
+  rowAmount = 2;
+  row;
+  itemWidth;
+  itemHeight;
 
-        this.item = /*html*/`
-             <div class="col">Hier könnte dein Name stehen</div>
+  
+  taskCounter;
+  img;
+  item;
+  index;
+  top;
+  left;
+
+  constructor(id, index) {
+    this.item = /*html*/ `
+             <div id="item${id}${index}" class="col">${description}</div>
         `;
-         this.itemRender(id);
-        }
-    
-        itemRender (id) {
-            docID(id).innerHTML = this.item;
-        }
+    this.itemRender(id, index);
+    this.calcPosition(index);
+    this.renderPosition(id, index);
+  }
+
+  itemRender(id, index) {
+    docID(`${id}${index}`).innerHTML = this.item;
+  }
+
+  renderPosition(id, index) {
+    docID(`item${id}${index}`).style.width = `${this.itemWidth}px`;
+    docID(`item${id}${index}`).style.left = `${this.left}px`;
+    docID(`item${id}${index}`).style.top = `${this.top}px`;
+  }
+
+  calcPosition(index) {
+    this.itemWidth = this.containerWidth / this.itemAmountPerRow - this.gap;
+    this.itemHeight =
+      (this.containerHeight - this.spanheight) / this.rowAmount - this.gap;
+
+    if (index == 0) {
+      let singleContainer = this.itemAmountPerRow - 1;
+      this.itemWidth = (this.containerWidth * singleContainer) / this.itemAmountPerRow - this.gap;
+      index = 0;
+      this.row = 0;
+    } else if (index == 1) {
+      index = this.itemAmountPerRow - 1;
+      this.row = 0;
+    } else {
+      index = index - 2;
+      this.row = 1;
+    }
+
+    this.top =
+      this.gap / 2 +
+      this.spanheight +
+      this.row * this.itemHeight +
+      this.gap * (this.rowAmount + 1);
+    this.left = this.gap / 2 + index * this.itemWidth + index * this.gap;
+  }
 }
