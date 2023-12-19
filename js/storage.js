a671e3d7bfbde2727a799f835e5c9d7839cc937f
Mina <mina_zarkesh@gmx.de>
2023-12-19 21:47:39 +0100

    update Overlays Contacts

--- js/storage.js
+++ js/storage.js
@@ -59,7 +59,7 @@ function createInputElements() {
     "inputimgdiv",
     "phone",
     "Phone",
-    "../assets/img/icon-call.svg.png"
+    "../assets/img/icon-call.svg"
   );
 
   inputPassword = new Divinputimg(
