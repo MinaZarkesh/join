1353750e35b0a3dff93948e46068dfc07e6ac3e8
UriOriKay <127621006+UriOriKay@users.noreply.github.com>
2023-12-19 21:30:54 +0100

    Assigned to

--- js/add_task.js
+++ js/add_task.js
@@ -11,18 +11,19 @@ function initAddtask() {
     new Urgencybtn('priority-button', "Urgent");
     new Urgencybtn('priority-button', "Medium");
     new Urgencybtn('priority-button', "Low");
-    new Divselect('contentCon', 'assigned', 'Assigned to', true, 'assigned-select');
-    new Selectoption('assigned-select', "selected", "Choose...");
-    new Selectoption('assigned-select', "value='1'", "Bild Vorname Nachname");
-    new Selectoption('assigned-select', "value='2'", "Bild Vorname Nachname");
-    new Selectoption('assigned-select', "value='3'", "Bild Vorname Nachname");
+    new Labeldiv('contentCon', 'assigned', 'Assigned to', true);
+    new Divinputimg('assigned', 'input-con', 'text', "Choose...", '../assets/img/arrow_drop_down.png');
+    // new Selectoption('assigned-select', "selected", "Choose...");
+    // new Selectoption('assigned-select', "value='1'", "Bild Vorname Nachname");
+    // new Selectoption('assigned-select', "value='2'", "Bild Vorname Nachname");
+    // new Selectoption('assigned-select', "value='3'", "Bild Vorname Nachname");
     new Divselect('contentCon', 'category', 'Category', false, 'category-select');
     new Selectoption('category-select', "selected", "Choose...");
     new Selectoption('category-select', "value='1'", "Bild Category");
     new Selectoption('category-select', "value='2'", "Bild Category");
     new Selectoption('category-select', "value='3'", "Bild Category");
     new Labeldiv('contentCon', 'subtask', 'Subtask', false);
-    new Divinputimg('subtask', 'subtask-input', 'text', 'Add new subtask', '../assets/img/+.png');
+    new Divinputimg('subtask', 'input-con', 'text', 'Add new subtask', '../assets/img/+.png');
     new Subtitles('contentCon', 'die-subtask', 'Dies ist eine subtask');
 }
 
