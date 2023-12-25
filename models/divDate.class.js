c308d1e538cb16f2b2767cbafc59390c7e97c9eb
UriOriKay <127621006+UriOriKay@users.noreply.github.com>
2023-12-25 09:02:45 +0100

    min date

--- models/divDate.class.js
+++ models/divDate.class.js
@@ -1,8 +1,21 @@
 class Divdate extends Labeldiv {
 
+    min_date;
     constructor(parent, child, name, optional) {
         super(parent, child, name, optional)
-        this.content = `<input type="date" class="input-field input-blue font-t6" name="" id="date-input">`
+        // min="2000-01-02"
+        this.min_date = this.today()
+        this.content = `<input type="date" min="${this.min_date}" class="input-field input-blue font-t6" name="" id="date-input">`
         this.render(child, this.content);
+    }
+
+    today(){
+        let time = new Date;
+        let year = time.getFullYear();
+        let month = time.getMonth() + 1;
+        let day = time.getDate()
+        let today = `${year}-${month}-${day}`; 
+
+        return today
     }
 }
