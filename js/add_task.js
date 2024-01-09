d307caadf80feb6ddc36c76b0d681495a22666f2
Mina <mina_zarkesh@gmx.de>
2024-01-09 14:03:11 +0100

    update mina

--- js/add_task.js
+++ js/add_task.js
@@ -93,7 +93,7 @@ function dropdownReset(parent, imgid) {
 }
 
 function createContactListTask() {
-    createContactBox();
+    createContactBox("contact-list-parent");
     let div_id;
     contacts.forEach((e) => {
         div_id = `contact-list-parent-div-${e.idx}`;
