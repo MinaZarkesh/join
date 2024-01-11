0b96edbdf9dda04aa469b7852a8e40452f705e94
Mina <mina_zarkesh@gmx.de>
2024-01-11 23:31:47 +0100

    update Navbar for not login

--- models/menuLink.class.js
+++ models/menuLink.class.js
@@ -6,17 +6,18 @@ class MenuLink{
     img;
     img_id;
     img_src;
-    constructor(id) {
+    parent;
+    constructor(parent, id) {
         // super();
-        this.classname = "active";
+        // this.classname = "active";
         this.name = id.charAt(0).toUpperCase() + id.slice(1);
         this.name = this.name.replace("_t", " T");
         this.anchor_id = `${id}-link`;
         this.anchor_href = `${id}.html`;
         this.img_id = `${this.anchor_id}-img`
         this.img_src = `../assets/img/${id}_menu.png`;
-        
-        new Anchor('navbar', this.anchor_id, this.anchor_class, this.anchor_href);
+        this.parent = parent
+        new Anchor(parent, this.anchor_id, this.anchor_class, this.anchor_href);
         this.img = new Img(this.anchor_id, this.img_id, undefined, this.img_src);
         new Span(this.anchor_id, "", "", this.name);
         docID(this.img_id).alt = id;
